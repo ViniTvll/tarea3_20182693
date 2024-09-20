47ef98b172a10ba56d9d273cf393c0b62b63ac93
ViniTvll <vinitavella18@gmail.com>
2024-09-20 18:59:49 -0500

    commit

--- src/main/java/com/example/tarea3_20182693/repository/EmployeeRepository.java
+++ src/main/java/com/example/tarea3_20182693/repository/EmployeeRepository.java
@@ -17,5 +17,20 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     @Query(nativeQuery = true, value = "SELECT * FROM hr.employees where firstName = ?1")
     List<Employee> buscarPorNombre(String textoIngreso);
 
+    @Query(nativeQuery = true, value = "SELECT * FROM hr.employees where lastName like %?1%")
+    List<Employee> buscarPorApellido(String apellido);
+
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE hr.employees SET firstName = ?1 WHERE EmployeeID = ?2")
+    void actualizarNombre(String firstName, int employeeId);
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE hr.employees SET lastName = ?1 WHERE EmployeeID = ?2")
+    void actualizarApellido(String lastName, int employeeId);
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE hr.employees SET email = ?1 WHERE EmployeeID = ?2")
+    void actualizarCorrreo(String email, int employeeId);
+
 
 }
